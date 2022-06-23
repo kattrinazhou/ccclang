04bd4d1144f030f262b0b762cfbb2eb3972cc01b
Xinuo zhou <2550838z@student.gla.ac.uk>
2022-06-23 13:44:17 +0100

    first commit

---
+++ helloworld.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+//this is the comment
+/*
+this is the first try of the c language */
+int main(){
+    printf("Hello World!\n zhou");
+
+int mybdate = 31;
+char mybmonth[] = "May";
+int mybyear = 2002;
+
+printf("My birthday data is : %d of %s", mybdate, mybmonth );
+return 0;}
