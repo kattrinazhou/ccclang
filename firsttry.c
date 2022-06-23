d42b1b3d5931422c1afcb825c5125c8c0463f6e1
Xinuo zhou <2550838z@student.gla.ac.uk>
2022-06-23 14:46:38 +0100

    addfunction

---
+++ firsttry.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+//main:表示一个主函数
+// int表示返回值为整数类型（integer）
+//main是函数的名称，这里是主函数，后面括号用来输入参数
+//花括号：内部是函数体
+int main () {
+    printf("Hello world\n");
+    //主函数返回值
+    //双斜杠表示注释，解释代码
+    return 0;
+}
+
